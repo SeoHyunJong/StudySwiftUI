406dd4f609ce00ff21a8a3981004d8dd4a908fd4
SeoHyunJong <shj971219@gmail.com>
2022-07-12 15:48:27 +0900

    Initial Commit

---
+++ WhatIsSwiftUI/WhatIsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatIsSwiftUIApp.swift
+//  WhatIsSwiftUI
+//
+//  Created by 서현종 on 2022/07/12.
+//
+
+import SwiftUI
+
+@main
+struct WhatIsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
